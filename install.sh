057b8c49c279024508431c35110dd86f51b902cd
Mattthew <matt@deloughry.com>
2019-02-10 10:18:54 +0000

    added i3 configs

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ link_if_missing $PWD/bin                    $HOME/bin
 link_if_missing $PWD/bash                   $HOME/.bash
 link_if_missing $PWD/zsh                    $HOME/.zsh
 link_if_missing $PWD/vim                    $HOME/.vim
-link_if_missing $PWD/vim/bundles.vim  
+link_if_missing $PWD/vim/bundles.vim
 link_if_missing $PWD/bashrc                 $HOME/.bashrc
 link_if_missing $PWD/ackrc                  $HOME/.ackrc
 link_if_missing $PWD/gitconfig              $HOME/.gitconfig
@@ -33,7 +33,10 @@ link_if_missing $PWD/tmux.conf              $HOME/.tmux.conf
 link_if_missing $PWD/vimrc                  $HOME/.vimrc
 link_if_missing $PWD/zshrc                  $HOME/.zshrc
 
-git clone https://github.com/riywo/ndenv  $HOME/.ndenv   
+link_if_missing $PWD/i3/config/config       $HOME/.config/i3/config
+link_if_missing $PWD/i3/i3status/config       $HOME/.config/i3status/config
+
+git clone https://github.com/riywo/ndenv  $HOME/.ndenv
 git clone https://github.com/riywo/node-build.git $(ndenv root)/plugins/node-build
 
 # Install vim plugins using Vundle
